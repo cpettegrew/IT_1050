ef7e88af3717416d4f2f5705e8df40bf34dca3a0
cpettegrew <31905853+cpettegrew@users.noreply.github.com>
2017-12-04 19:13:34 -0500

    Add files via upload
    Lab 2

---
+++ Lab2/Lab2/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Person
+    {
+        public int Age;
+        public string firstName;
+        public string lastName;
+        public Person spouse;
+
+    }
+}
